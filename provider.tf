24654a51a91cb7322fb31c76d270f8177dd99528
Sai Gopinath <sainath.gopinath@gmail.com>
2025-07-20 15:31:34 -0400

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 locals {
   region        = "ca-central-1"
-  name          = "teluskoQ-cluster"
+  name          = "pipeline-cluster"
   vpc_cidr      = "10.123.0.0/16"
   azs           = ["ca-central-1a", "ca-central-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
